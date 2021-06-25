9ec4ff4cfe066e9f393be1c57b82622794be2813
Joe Cserko <j.cserko@gmail.com>
2021-06-25 14:50:37 +0200

    feat: init

---
+++ JS-task/task02-dom/dom.js
@@ -0,0 +1,19 @@
+const bookList = [
+    { title: 'Don Quixote', isRead: true, author: 'Miguel de Cervantes' },
+    { title: 'Lord of the Rings', isRead: false, author: 'J.R.R. Tolkien' },
+    { title: 'Harry Potter and the Sorcerers Stone', isRead: true, author: ' J.K. Rowling' },
+    { title: 'And Then There Were None', isRead: true, author: 'Agatha Christie' },
+    { title: 'Alices Adventures in Wonderland', isRead: false, author: 'Lewis Carroll' },
+    { title: 'The Lion, the Witch, and the Wardrobe', isRead: true, author: 'C.S. Lewis' },
+    { title: 'Love in the Time of Cholera', isRead: true, author: 'Gabriel García Márquez' },
+]
+
+function listMyBooks(bookList) {
+
+}
+
+function configStyle() {
+
+}
+
+export { listMyBooks, configStyle };
